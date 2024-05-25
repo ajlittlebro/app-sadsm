bc2edbdf0d549ffb83e4275c550a4d6afeb4771e
ajlittlebro <nanain.mozo3@gmail.com>
2024-05-25 02:03:37 -0600

    refactor: Improve Clients CRUD API and context functions

--- server/sql/db.sql
+++ server/sql/db.sql
@@ -1,103 +1,105 @@
-CREATE TABLE `CLIENTE`(
-  `idCliente` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Apellido` VARCHAR(50) NOT NULL,
-  `Correo electronico` VARCHAR(50) NOT NULL,
-  `Telefono` INT(10) NOT NULL,
-  `Direccion` VARCHAR(50) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO CLIENTE(Nombre, Apellido, `Correo electronico`, Telefono, Direccion) VALUES
-  ('Cliente', 'Ejemplo', 'cliente@ejemplo.com', 1234567890, 'Direccion Cliente Ejemplo');
+CREATE DATABASE IF NOT EXISTS `db-sadsm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db-sadsm`;
 
-CREATE TABLE `EMPLEADO`(
-  `idEmpleado` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Apellido` VARCHAR(50) NOT NULL,
-  `Correo` VARCHAR(50) NOT NULL,
-  `Telefono` INT(10) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Correo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Direccion` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO EMPLEADO(Nombre, Apellido, Correo, Telefono) VALUES
-  ('Empleado', 'Ejemplo', 'empleado@ejemplo.com', 1234567890);
+CREATE TABLE IF NOT EXISTS `cotizacion` (
+  `idCot` int NOT NULL AUTO_INCREMENT,
+  `idCliente` int NOT NULL,
+  `idEvento` int NOT NULL,
+  `Fecha` date NOT NULL,
+  `Monto` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idCot`),
+  KEY `idCliente` (`idCliente`),
+  KEY `idEvento` (`idEvento`),
+  CONSTRAINT `cotizacion_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
+  CONSTRAINT `cotizacion_ibfk_2` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `SALON`(
-  `idSalon` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Capacidad` int(10) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `detallepc` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Telefono` int NOT NULL,
+  `idPago` int NOT NULL,
+  `Monto` decimal(10,2) NOT NULL,
+  `Forma_de_Pago` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`idCliente`),
+  KEY `idPago` (`idPago`),
+  CONSTRAINT `detallepc_ibfk_1` FOREIGN KEY (`idPago`) REFERENCES `pago` (`idPago`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO SALON(Nombre, Capacidad) VALUES
-  ('Salon Ejemplo', 500);
+CREATE TABLE IF NOT EXISTS `empleado` (
+  `idEmpleado` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `Telefono` int NOT NULL,
+  PRIMARY KEY (`idEmpleado`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `EVENTO`(
-  `idEvento` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Descripccion` VARCHAR(90) NOT NULL,
-  `Fecha` DATE NOT NULL,
-  `Hora` TIME(0) NOT NULL,
-  `Lugar` VARCHAR(50) NOT NULL,
-  `Salon` VARCHAR(50) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `evento` (
+  `idEvento` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Descripcion` varchar(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Lugar` varchar(50) NOT NULL,
+  `Salon` varchar(50) NOT NULL,
+  PRIMARY KEY (`idEvento`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO EVENTO(Nombre, Descripccion, Fecha, Hora, Lugar, Salon) VALUES
-  ('Evento Ejemplo', 'Descripccion de evento ejemplo', '2024-05-01', '09:10:00', 'Lugar de Evento Ejemplo', 'Salon Ejemplo');
+CREATE TABLE IF NOT EXISTS `historial` (
+  `idHist` int NOT NULL AUTO_INCREMENT,
+  `idEvento` int NOT NULL,
+  `Fecha` date NOT NULL,
+  `Asistencia` varchar(50) NOT NULL,
+  `Pagos` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idHist`),
+  KEY `idEvento` (`idEvento`),
+  CONSTRAINT `historial_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `PAQUETE`(
-  `idPaquete` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Descripccion` VARCHAR(90) NOT NULL,
-  `Precio` INT(15) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `pago` (
+  `idPago` int NOT NULL AUTO_INCREMENT,
+  `Fecha` date NOT NULL,
+  `Monto` decimal(10,2) NOT NULL,
+  `Forma_de_Pago` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`idPago`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO PAQUETE(Nombre, Descripccion, Precio) VALUES
-  ('Paquete Ejemplo', 'Descripccion del Paquete Ejemplo', 1500);
+CREATE TABLE IF NOT EXISTS `paquete` (
+  `idPaquete` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Descripccion` varchar(90) NOT NULL,
+  `Precio` int NOT NULL,
+  PRIMARY KEY (`idPaquete`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `PAGO`(
-  `idPago` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Fecha` DATE NOT NULL,
-  `Monto` DECIMAL(10,2) NOT NULL,
-  `Forma de Pago` VARCHAR(50) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `salon` (
+  `idSalon` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Capacidad` int NOT NULL,
+  PRIMARY KEY (`idSalon`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO PAGO(Fecha, Monto, `Forma de Pago`) VALUES
-  ('2024-05-01', 1500.00, 'Ejemplo Forma de Pago');
-
-CREATE TABLE `COTIZACION`(
-  `idCot` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `idCliente` INT(10) NOT NULL,
-  FOREIGN KEY(idCliente) REFERENCES CLIENTE(idCliente),
-  `idEvento` INT(10) NOT NULL,
-  FOREIGN KEY(idEvento) REFERENCES EVENTO(idEvento),
-  `Fecha` DATE NOT NULL,
-  `Monto` DECIMAL(10,2) NOT NULL
-);
-
-INSERT INTO COTIZACION(idCliente, idEvento, Fecha, Monto) VALUES
-  (1, 1, '2024-05-01', 1500.00);
-
-CREATE TABLE `HISTORIAL`(
-  `idHist` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `idEvento` INT(10) NOT NULL,
-  FOREIGN KEY(idEvento) REFERENCES EVENTO(idEvento),
-  `Fecha` DATE NOT NULL,
-  `Asistencia` VARCHAR(50) NOT NULL,
-  `Pagos` DECIMAL(10,2) NOT NULL
-);
-
-INSERT INTO HISTORIAL(idEvento, Fecha, Asistencia, Pagos) VALUES
-  (1, '2024-05-01', 'Todos', 1.00);
-
-CREATE TABLE `DETALLEPC`(
-  `idCliente` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Apellido` VARCHAR(50) NOT NULL,
-  `Telefono` INT(10) NOT NULL,
-  `idPago` INT(10) NOT NULL,
-  FOREIGN KEY(idPago) REFERENCES PAGO(idPago),
-  `Monto` DECIMAL(10,2) NOT NULL,
-  `Forma de Pago` VARCHAR(50) NOT NULL
-);
-
-INSERT INTO DETALLEPC(Nombre, Apellido, Telefono, idPago, Monto, `Forma de Pago`) VALUES
-  ('Nombre', 'Ejemplo', 1234567890, 1, 1500.00, "Forma de pago Ejemplo");
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
